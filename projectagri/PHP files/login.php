ac74a69c5d42450530d603c33217b7c4aea8fd24
keyur Desai <91608398+keyurdesai-08@users.noreply.github.com>
2023-10-16 12:01:50 +0530

    Add files via upload

---
+++ projectagri/PHP files/login.php
@@ -0,0 +1,31 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="background-image: url(../agri.jpg); background-size:cover;">
+  <div class="header" style="background-color: green;">
+  	<h2>Login</h2>
+  </div>
+	 
+  <form method="post" action="login.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  		<label>Username</label>
+  		<input type="text" name="username" >
+  	</div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="password" name="password">
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user" style="background-color: green;">Login</button>
+  	</div>
+  	<p>
+  		Not yet a member? <a href="register.php">Sign up</a>
+  	</p>
+  </form>
+</body>
+</html>
